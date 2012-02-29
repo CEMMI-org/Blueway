7d54e61d91c371df0412e0952f3eaae65c188ea5
cemmi <cemmi@netbook11.local>
2012-02-28 19:56:40 -0500

    Adding seth

--- config/__init__.py
+++ config/__init__.py
@@ -4,7 +4,7 @@ MODES = ["FILE","WEB","LIVE","GAME"]
 
 class CONFIG:
      # general settings
-     mode = 1 # 0=render to file,1=websocket, 2=live to lights, 3=pygame
+     mode = 2 # 0=render to file,1=websocket, 2=live to lights, 3=pygame
      disableColormap = True
 
      #sim settings
